a207e04cdb588beeffcc9be0d5867aae9ea40f50
Andrew Halle <ahalle@berkeley.edu>
2020-05-17 11:28:45 -0700

    add skeleton for new runtime call function

--- main.c
+++ main.c
@@ -6,10 +6,20 @@
 // typedef int (*add_func)(int, int);
 
 typedef struct {
-  int x;
-  int y;
   void *func;
+  int *args;
+  int n_args;
 } callable;
+
+void init_callable(callable *c) {
+  c->args = (int*) malloc(16 * sizeof(int));
+  c->n_args = 0;
+}
+
+void add_arg_callable(callable *c, int arg) {
+  c->args[c->n_args] = arg;
+  c->n_args++;
+}
 
 // this is the function we'll write in assembly
 // takes a pointer to a callable, and a pointer to where
@@ -19,7 +29,12 @@ void runtime_call(void *c, void *ret);
 int main(void) {
   void *handle = dlopen("./libadd.so", RTLD_NOW);
   void *add = dlsym(handle, "add");
-  callable c = { 1, 2, add };
+
+  callable c = { add };
+  init_callable(&c);
+  add_arg_callable(&c, 1);
+  add_arg_callable(&c, 2);
+
   int x;
   runtime_call(&c, &x);
   printf("1 + 2 = %d\n", x);
--- runtime-call.s
+++ runtime-call.s
@@ -1,7 +1,7 @@
 global runtime_call
 
 section .text
-runtime_call: push    rbp
+runtime_call:  push    rbp
                mov     rbp, rsp
                sub     rsp, 48
                mov     QWORD [rbp-40], rdi
@@ -26,3 +26,21 @@ runtime_call: push    rbp
                nop
                leave
                ret
+
+				while (n_args > 6) {
+					push    6           ; push additional arguments onto the stack
+					n_args--            ; don't actually decrement n_args, make a copy
+				}
+
+				jmp     6 - n_args    ; if we have 6 args, this doesn't jump and pushes
+				                      ; into all registers
+															; if we have less than 6 arguments, this skips the
+															; unneeded registers
+
+        mov     r9d, 5 ; order of registers is the calling convention
+        mov     r8d, 4
+        mov     ecx, 3
+        mov     edx, 2
+        mov     esi, 1
+        mov     edi, 0
+
